983880a25c153b4c07cca48d686d141fbde54315
Wajahat Karim <wajahatkarim3@gmail.com>
2018-02-07 23:47:44 +0500

    added clapfab module...

---
+++ clapfab/src/main/java/com/wajahatkarim3/clapfab/ClapFAB.kt
@@ -0,0 +1,13 @@
+package com.wajahatkarim3.clapfab
+
+import android.content.Context
+import android.support.design.widget.FloatingActionButton
+import java.util.jar.Attributes
+
+/**
+ * Created by wajah on 2/7/2018.
+ */
+class ClapFAB(context: Context?) : FloatingActionButton(context)
+{
+
+}
