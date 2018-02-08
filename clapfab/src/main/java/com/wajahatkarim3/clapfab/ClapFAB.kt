b9edd63ab89229cb2a04f02337e562e979c85fff
Wajahat Karim <wajahatkarim3@gmail.com>
2018-02-09 02:13:35 +0500

    fab in progress

--- clapfab/src/main/java/com/wajahatkarim3/clapfab/ClapFAB.kt
+++ clapfab/src/main/java/com/wajahatkarim3/clapfab/ClapFAB.kt
@@ -2,12 +2,47 @@ package com.wajahatkarim3.clapfab
 
 import android.content.Context
 import android.support.design.widget.FloatingActionButton
+import android.util.AttributeSet
+import android.widget.FrameLayout
 import java.util.jar.Attributes
 
 /**
  * Created by wajah on 2/7/2018.
  */
-class ClapFAB(context: Context?) : FloatingActionButton(context)
+class ClapFAB : FrameLayout
 {
+    val TAG = ClapFAB::class.simpleName
+
+    constructor(context: Context?) : super(context)
+    {
+        init(context, null)
+    }
+
+    constructor(context: Context?, attributes: AttributeSet) : super(context, attributes)
+    {
+        init(context, attributes)
+    }
+
+    constructor(context: Context?, attributeSet: AttributeSet, defStyleAttr: Int) : super(context, attributeSet, defStyleAttr)
+    {
+        init(context, attributeSet)
+    }
+
+    fun init(context: Context?, attributes: AttributeSet?)
+    {
+        // Set Default Values Here
+
+
+        // Check for attributes
+        if (attributes != null)
+        {
+            //val typedArray = context?.obtainStyledAttributes(attributes, R.styleable.clap_fab, 0, 0)
+
+        }
+
+        //
+    }
+
+
 
 }
