a61bf4a581d0766c973a3419af232a36b4e5829a
Wajahat Karim <wajahatkarim3@gmail.com>
2018-02-14 23:41:44 +0500

    added data binding... and working and demo first

--- app/src/main/java/com/wajahatkarim3/mediumclap_android/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/mediumclap_android/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.wajahatkarim3.mediumclap_android
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.wajahatkarim3.mediumclap_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bi: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
