eab7a7a0e59cbaa64da520bce79cd50b10992cf3
Wajahat Karim <wajahatkarim3@gmail.com>
2018-06-09 00:14:59 +0500

    basic counter animation working

--- app/src/main/java/com/wajahatkarim3/mediumclap_android/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/mediumclap_android/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.wajahatkarim3.mediumclap_android
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.databinding.DataBindingUtil
+import android.databinding.adapters.ViewGroupBindingAdapter.setListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import com.wajahatkarim3.mediumclap_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var bi: ActivityMainBinding
     var clapCount = 0
+    var initialCountY = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +43,44 @@ class MainActivity : AppCompatActivity() {
     {
         bi.txtCountCircle.text = "+" + clapCount.toString()
         bi.txtCountCircle.visibility = View.VISIBLE
+
+        var showAnim = ObjectAnimator.ofFloat(bi.txtCountCircle, "translationY", -200f)
+        var hideAnim = ObjectAnimator.ofFloat(bi.txtCountCircle, "alpha", 0f)
+
+        /*
+        var hideAnim = bi.txtCountCircle.animate()
+                .alpha(0f)
+                .setInterpolator(DecelerateInterpolator())
+                .setDuration(150)
+                */
+
+        var animSet = AnimatorSet()
+        animSet.playSequentially(showAnim, hideAnim)
+        animSet.addListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                resetCircleCountTextView()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+        })
+        animSet.start()
+    }
+
+    fun resetCircleCountTextView()
+    {
+        bi.txtCountCircle.y = bi.fabDemoClap.y
+        bi.txtCountCircle.visibility = View.GONE
+        bi.txtCountCircle.alpha = 1f
     }
 }
