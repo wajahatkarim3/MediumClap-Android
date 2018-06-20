2182b55fcf649da1c4e4fee069624124d71ac195
Wajahat Karim <wajahatkarim3@gmail.com>
2018-06-20 11:50:18 +0500

    Created a simple demo. Fixes issue #3

---
+++ app/src/main/java/com/wajahatkarim3/mediumclap_android/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.wajahatkarim3.mediumclap_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
